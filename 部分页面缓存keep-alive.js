e1d6090f40aaf31e6520b9e522c876bb794648aa
521xuxiao <37203926+521xuxiao@users.noreply.github.com>
2021-08-05 13:19:41 +0800

    Add files via upload

---
+++ 部分页面缓存keep-alive.js
@@ -0,0 +1,30 @@
+
+/**
+ *   B页面跳转到A页面， A页面缓存， 其它的跳转到A页面， A页面不缓存
+ */
+
+{
+    path: '/A',
+	name: 'A',
+    component: A,
+    meta: {
+        title: "A",
+        keepAlive: true
+    }
+},
+{
+    path: '/B',
+    name: 'B',
+    component: B
+},
+
+
+//	A.vue中
+beforeRouteLeave(to, from, next) {
+    if (to.name === "B") {             // 跳转到 B 页面
+        from.meta.keepAlive = true;    //  A 页面被缓存下来
+    } else {                           // 跳转到其它的页面
+        from.meta.keepAlive = false;   // A 页面不被缓存
+    }
+    next();
+}
